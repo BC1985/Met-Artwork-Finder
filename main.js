a6f684ca6fef03b01e0186a07edc963efcfdff10
Ben Cohen <benjcohen1985@gmail.com>
2019-03-20 11:39:24 -0400

    add landing page, banner, modify and add JS functions

--- main.js
+++ main.js
@@ -4,7 +4,38 @@ const baseUrl = 'https://collectionapi.metmuseum.org/public/collection/v1/search
 // Array for iterating through index items
 let imageIndex = 0
 let objIDs = [];
+// Global variable for image counter string
 let search = ''
+
+
+// from landing screen
+
+function takeToSearchEngine(){
+    $('#form-link').on('click', function(){
+        $('form').removeClass('hidden');
+        $('.hero').hide();
+        $('header').css('background','none');
+        $('header').css('margin-top', '30px');
+        //$('.top-header').removeClass('hidden');
+        $('.top-header').css('display', 'block');
+        changeBackground();
+    });
+
+}
+
+function changeBackground(){
+     $('form').prepend(`
+     
+         <p class="small">Enter keyword to browse artworks</p>
+    `)
+    // $('.top-header').append(`
+    // <img src="images/art-creative-creativity-20967.jpg" class='img-big'>
+    // `);
+    $('body').css('background-color', '#4E4343');
+    $('body').css('background-image', 'none');
+
+
+}
 // When user clicks submit button
 function watchForm() {
     $('form').submit(event => {
@@ -36,7 +67,8 @@ const searching = 'Searching...'
         .catch(err => {
             $('.info').empty();
             $('#message').text(`${error}`);
-            $('#search-term').val('')
+            $('#search-term').val('');
+            $('.keyword').empty();
         });       
 }
 // Get array of keyword-related items
@@ -75,10 +107,9 @@ function showResults(responseJson) {
     // Empties container from previous searches
     $('.results').empty();
     $('.info').empty();
-    $('.keyword').text(`Results for "${search}". Item no.${imageIndex + 1} out of ${objIDs.length}`)
-    // Displays images
+    $('.keyword').text(`Results for "${search}". Item no. ${imageIndex + 1}/ ${objIDs.length}`)
+    // Display image and open in lightbox on click
     $('.results').append(
-        
         `<a data-lightbox="${keywordMatchArray.primaryImage}" href="${keywordMatchArray.primaryImage}"><img src=${keywordMatchArray.primaryImage} class='img'></a>
         <p class="small">Click picture for full size image</p>
         `)
@@ -97,7 +128,6 @@ function showResults(responseJson) {
     <button id='next'>Next item</button>
     `   
     );   
-       //console.log(url)
         // Fetches the next index in the array when clicking the 'next' button
     $('#next').on('click', function(){
     imageIndex++;
@@ -108,7 +138,8 @@ function showResults(responseJson) {
 }
 
 function renderPage() {
-    watchForm();   
+    watchForm();
+    takeToSearchEngine();   
 }
 
 $(renderPage)
